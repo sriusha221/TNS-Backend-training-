97f9087840de7c024390c98e5ffde4ff6921582c
sri usha <sri usha@192.168.1.7>
2025-08-05 22:06:44 +0530

    Initial commit

---
+++ Example/src/variableDemomain.java
@@ -0,0 +1,15 @@
+//Demo for variable main function 
+public class variableDemomain {
+
+	public static void main(String[]args) {
+	      variableDemo ob=new variableDemo();
+	      System.out.println(ob.name="This is instance variable");
+	      ob.display();
+	      
+	      //call method to print local variable
+	      System.out.println(variableDemo.college);
+	      //print static variable
+		}
+
+	}
+
