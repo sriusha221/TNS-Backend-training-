6bf0f91a766f8377eeac849d4e177647b689316b
sri usha <sri usha@192.168.1.4>
2025-08-12 20:42:36 +0530

    2 commit

--- Student/src/Studentmain.java
+++ Student/src/Studentmain.java
@@ -1,25 +1,27 @@
 //student main function
 public class Studentmain {
  
-public class StudentMain {
+
  public static void main(String[] args) {
-     StudentDemo ob = new StudentDemo();
-     ob.rollno = 100;
-     ob.sname = "Usha";
-     ob.sbranch = "CSE";
-     ob.print();
+   
+        StudentDemo ob = new StudentDemo();
+        ob.rollno = 100;
+        ob.sname = "Usha";
+        ob.sbranch = "CSE";
+        ob.print();
 
-     StudentDemo o = new StudentDemo();
-     o.rollno = 101;
-     o.sname = "Shivapriya";
-     o.sbranch = "CSE";
-     o.print();
+        StudentDemo o = new StudentDemo();
+        o.rollno = 101;
+        o.sname = "Shivapriya";
+        o.sbranch = "CSE";
+        o.print();
 
-     StudentDemo obj = new StudentDemo();
-     obj.rollno = 102;
-     obj.sname = "Geetha";
-     obj.sbranch = "DS";
-     obj.print();
- }
+        StudentDemo obj = new StudentDemo(); // fixed case
+        obj.rollno = 102;
+        obj.sname = "Geetha";
+        obj.sbranch = "DS";
+        obj.print();
+    }
 }
-}
+
+
