1027cd959318f38d3c6bf32d96bd3369895f666b
Sri Usha <polojusriusha321@gmail.com>
2025-09-18 20:36:06 +0530

    Add files via upload

---
+++ StudentDemo.java
@@ -0,0 +1,12 @@
+//Demo for student keyword
+public class StudentDemo {
+  
+ int rollno;
+ String sname;
+ String sbranch;
+ static String collegename = "KPRIT"; // Fixed 'Static' and space in variable name
+
+ void print() {
+     System.out.println("The details of the student are: " + rollno + " " + sname + " " + sbranch + " " + collegename);
+ }
+}
---
+++ Studentmain.java
@@ -0,0 +1,26 @@
+//student main function
+public class Studentmain {
+ 
+ public static void main(String[] args) {
+   
+        StudentDemo ob = new StudentDemo();
+        ob.rollno = 100;
+        ob.sname = "Usha";
+        ob.sbranch = "CSE";
+        ob.print();
+
+        StudentDemo o = new StudentDemo();
+        o.rollno = 101;
+        o.sname = "Shivapriya";
+        o.sbranch = "CSE";
+        o.print();
+
+        StudentDemo obj = new StudentDemo(); // fixed case
+        obj.rollno = 102;
+        obj.sname = "Geetha";
+        obj.sbranch = "DS";
+        obj.print();
+    }
+}
+
+
